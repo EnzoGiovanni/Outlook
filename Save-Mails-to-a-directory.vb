7b084a105b2c7dc5c48fa4678a3990f46dc17df3
EnzoGiovanni <EnzoGiovanni@users.noreply.github.com>
2015-09-17 11:31:31 +0200

    Update Save-Mails-to-a-directory.vb

--- Save-Mails-to-a-directory.vb
+++ Save-Mails-to-a-directory.vb
@@ -3,8 +3,8 @@ Sub SaveMail()
     Dim SelectedMsg As Outlook.Selection: Set SelectedMsg = Application.ActiveExplorer.Selection
     If SelectedMsg.Count > 0 Then
         Dim Message As MailItem
-        For Each Message In SelectedMsg.MailItem
-            Trash = Message.SaveAs(Directory & SelectedMsg(elt).ConversationTopic & ".msg", OlSaveAsType.olMSG)
+        For Each Message In SelectedMsg
+            Message.SaveAs Directory & Message.ConversationTopic & ".msg", OlSaveAsType.olMSG
         Next Message
     End If
 End Sub
